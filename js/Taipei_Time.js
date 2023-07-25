fd92b078c1a00e82888e93885660f0b83342254b
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2023-07-26 02:46:48 +0800

    Update Taipei_Time.js

--- js/Taipei_Time.js
+++ js/Taipei_Time.js
@@ -2,5 +2,5 @@ setInterval(Time_now);
 
 function Time_now() {
 	let date = new Date();
-	document.getElementById("time").innerHTML ='板橋標準時間：'+date.toLocaleString('zh-TW',{hourCycle:'h23',timeZone:'Asia/Taipei'});
+	document.getElementById("time").innerHTML ='板橋標準時間：'+date.toLocaleString('sv-SE',{hourCycle:'h23',timeZone:'Asia/Taipei'});
 }
