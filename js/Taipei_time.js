c8f5cdf03d47a54d59489d0c54c47c9de0a1307c
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2023-07-26 02:18:21 +0800

    Delete Taipei_time.js

--- js/Taipei_time.js
+++
@@ -1,6 +0,0 @@
-setInterval(Time_now);
-
-function Time_now() {
-	let date = new Date();
-	document.getElementById("time").innerHTML ='板橋標準時間：'+date.toLocaleString('zh-TW',{hourCycle:'h23',timeZone:'Asia/Taipei'});
-}
